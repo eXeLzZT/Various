8177cdcdbb8b5412f7f3d702126e784dd0afc0b7
Fabian Böhme <boehme.fabian96@gmail.com>
2025-07-08 18:40:08 +0200

    Implements enum localization utils

---
+++ Various.Utils/Enums/LocalizedDescriptionAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Resources;
+
+namespace Various.Utils.Enums
+{
+    public class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        private readonly ResourceManager _resourceManager;
+        private readonly string _resourceKey;
+
+        public override string Description
+        {
+            get
+            {
+                var description = _resourceManager.GetString(_resourceKey);
+
+                return string.IsNullOrWhiteSpace(description) ? $"[[{_resourceKey}]]" : description;
+            }
+        }
+
+        public LocalizedDescriptionAttribute(string resourceKey, Type resourceType)
+        {
+            _resourceManager = new ResourceManager(resourceType);
+            _resourceKey = resourceKey;
+        }
+    }
+}
